fd10b0614b74c65d1d06fd5bdbb80f5c7e3f548e
uztm <tmcoderuz@gmail.com>
2023-09-01 20:53:33 +0500

    second

--- scrips.js
+++ scrips.js
@@ -5,3 +5,31 @@ document.getElementById('burgerButton').addEventListener('click', function () {
         span.classList.toggle('active');
     });
 });
+
+
+const botToken = 'YOUR_BOT_TOKEN';
+
+document.getElementById('telegramForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const message = `Name: ${name}\nPhone: ${phone}`;
+
+    const url = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=YOUR_CHAT_ID&text=${encodeURIComponent(message)}`;
+
+    fetch(url, { method: 'POST' })
+        .then(response => response.json())
+        .then(data => {
+            if (data.ok) {
+                alert('Your message has been sent to the bot.');
+                // You can redirect the user or perform other actions here if needed.
+            } else {
+                alert('An error occurred while sending the message to the bot.');
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            alert('An error occurred while sending the message to the bot.');
+        });
+});
